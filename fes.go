8fc8abdb54e5defcea96aed57f62749ff7da40dd
v_frgfeng <v_frgfeng@tencent.com>
2023-12-27 14:49:20 +0800

    update

--- fes.go
+++ fes.go
@@ -1,12 +1,48 @@
 package fesgo
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 type Engine struct {
 	router
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := &Context{W: w, R: r}
+	method := r.Method
+	for _, group := range e.routerGroups {
+		for name, handleFuncMap := range group.handleFuncMap {
+			url := "/" + group.name + name
+			// 路由匹配
+			if r.RequestURI != url {
+				continue
+			}
+
+			// 优先匹配 Any
+			handleFunc, ok := handleFuncMap[ANY]
+			if ok {
+				handleFunc(ctx)
+				return
+			}
+
+			// method 匹配
+			handleFunc, ok = handleFuncMap[method]
+			if ok {
+				handleFunc(ctx)
+				return
+			}
+
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "%s %s not allowed", r.RequestURI, method)
+			return
+		}
+	}
+	// 路由匹配失败
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "%s %s not found", r.RequestURI, method)
 }
 
 func NewEngine(port string) *Engine {
@@ -16,12 +52,8 @@ func NewEngine(port string) *Engine {
 }
 
 func (e *Engine) Run() {
-	for _, group := range e.routerGroups {
-		for path, handlefunc := range group.handleFuncMap {
-			http.HandleFunc("/"+group.name+path, handlefunc)
-		}
-	}
 
+	http.Handle("/", e)
 	err := http.ListenAndServe(":8111", nil)
 	if err != nil {
 		log.Fatal(err)
--- router.go
+++ router.go
@@ -1,17 +1,12 @@
 package fesgo
 
-import "net/http"
+import (
+	"net/http"
+)
 
-type HandleFunc func(w http.ResponseWriter, r *http.Request)
+const ANY = "ANY"
 
-type routerGroup struct {
-	name          string // 组名
-	handleFuncMap map[string]HandleFunc
-}
-
-func (r *routerGroup) Add(name string, handleFunc HandleFunc) {
-	r.handleFuncMap[name] = handleFunc
-}
+type HandleFunc func(ctx *Context)
 
 type router struct {
 	routerGroups []*routerGroup
@@ -19,10 +14,46 @@ type router struct {
 
 func (r *router) Group(name string) *routerGroup {
 	group := &routerGroup{
-		name:          name,
-		handleFuncMap: make(map[string]HandleFunc),
+		name:            name,
+		handleFuncMap:   make(map[string]map[string]HandleFunc),
+		handleMethodMap: make(map[string][]string),
 	}
 	r.routerGroups = append(r.routerGroups, group)
 
 	return group
+}
+
+type routerGroup struct {
+	name            string // 组名
+	handleFuncMap   map[string]map[string]HandleFunc
+	handleMethodMap map[string][]string
+}
+
+func (r *routerGroup) handle(name, method string, handleFunc HandleFunc) {
+	_, ok := r.handleFuncMap[name]
+	if !ok {
+		r.handleFuncMap[name] = make(map[string]HandleFunc)
+	}
+	_, ok = r.handleFuncMap[name][method]
+	if ok {
+		panic("路由已存在")
+	}
+	r.handleFuncMap[name][method] = handleFunc
+	r.handleMethodMap[method] = append(r.handleMethodMap[method], name)
+}
+
+func (r *routerGroup) Any(name string, handleFunc HandleFunc) {
+	r.handle(name, ANY, handleFunc)
+}
+func (r *routerGroup) Get(name string, handleFunc HandleFunc) {
+	r.handle(name, http.MethodGet, handleFunc)
+}
+func (r *routerGroup) Post(name string, handleFunc HandleFunc) {
+	r.handle(name, http.MethodPost, handleFunc)
+}
+func (r *routerGroup) Put(name string, handleFunc HandleFunc) {
+	r.handle(name, http.MethodPut, handleFunc)
+}
+func (r *routerGroup) Delete(name string, handleFunc HandleFunc) {
+	r.handle(name, http.MethodDelete, handleFunc)
 }
