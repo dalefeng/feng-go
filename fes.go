e919f41c2d43c994e95d4a908a62d356e4d31be5
v_frgfeng <v_frgfeng@tencent.com>
2024-03-04 15:32:03 +0800

    :sparkles: goroutine pool

--- fes.go
+++ fes.go
@@ -113,10 +113,22 @@ func (e *Engine) Run() {
 	}
 }
 
+func (e *Engine) RunTLS(addr string, certFile, keyFile string) {
+	http.Handle("/", e)
+	err := http.ListenAndServeTLS(addr, certFile, keyFile, e.Handler())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func (e *Engine) Use(middlewareFunc ...MiddlewareFunc) {
 	e.middles = middlewareFunc
 }
 
 func (e *Engine) RegisterErrorHandler(handler ErrorHandler) {
 	e.errorHandler = handler
+}
+
+func (e *Engine) Handler() http.Handler {
+	return e
 }
